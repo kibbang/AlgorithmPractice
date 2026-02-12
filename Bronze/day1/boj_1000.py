cbded8c77103b095db345692d02353753d1a9488
unknown <007kibbang@gmail.com>
2026-02-12 23:50:20 +0900

    first commit

---
+++ Bronze/day1/boj_1000.py
@@ -0,0 +1,6 @@
+def add():
+    a,b = map(int, input().split())
+    print(a+b)
+
+if __name__ == "__main__":
+    add()
---
+++ Bronze/day1/boj_10718.py
@@ -0,0 +1,6 @@
+def we_love_kriii():
+    print("강한친구 대한육군")
+    print("강한친구 대한육군")
+
+if __name__ == '__main__':
+    we_love_kriii()
